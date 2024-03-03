8f99688429e201fcac58acca175f1f002511398d
freddy andres <108620989+freddyandreszambrano@users.noreply.github.com>
2024-03-03 11:28:07 -0500

    Add files via upload

---
+++ RRHH_DISENO/making.js
@@ -0,0 +1,15 @@
+let btnAbrirPopup = document.getElementById('btn-abrir-popup'),
+            overlay = document.getElementById('overlay'),
+            popup = document.getElementById('popup'),
+            btnCerrarpopup = document.getElementById('btn-cerrar-popup');
+
+
+    btnAbrirPopup.addEventListener('click', function() {
+        overlay.classList.add('active');
+        popup.classList.add('active');
+    });
+
+    btnCerrarpopup.addEventListener('click', function() {
+        overlay.classList.remove('active');
+        popup.classList.remove('active');
+    });
